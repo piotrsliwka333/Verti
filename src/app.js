e29249ecdaf939cf2162b36d36e1560abbbd55e7
Piotr Sliwka <piotrsliwka333@gmail.com>
2020-07-07 12:05:25 +0200

    finished navigation
    navigation for mobile and desktop breakpoint on md = 960px

---
+++ src/app.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded',function () {
+	const $body = document.querySelector('body');
+	const $toggleBtn = document.querySelector('.toggle-menu');
+	const $pageHeder = document.querySelector('.page-header');
+	const $pageNav = document.querySelector('.nav')
+	const $dropDownBtn = document.querySelector('#drop-down');
+	const $dropDownMenu = document.querySelector('#drop-down-menu');
+
+
+	$toggleBtn.addEventListener('click',function () {
+		$pageHeder.classList.toggle('page-heder-active-nav')
+		$pageNav.classList.toggle('nav-active')
+		// when we remove class imediatly where will show white belt on the bottom coz our animation take 0.3ms
+		if($body.classList.contains('active-nav-body')) {
+			setTimeout(() => {
+				$body.classList.toggle('active-nav-body')
+			},600)
+		} else {
+			$body.classList.toggle('active-nav-body')
+		}
+	})
+
+	$dropDownBtn.addEventListener('click',() => {
+		$dropDownMenu.classList.toggle('active-drop-down-menu')
+	})
+})
