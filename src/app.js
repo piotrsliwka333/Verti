465b94c07bcc997e28499bccab1389dfa92d74bc
Piotr Sliwka <piotrsliwka333@gmail.com>
2020-07-07 20:02:42 +0200

    finished footers section

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded',function () {
+document.addEventListener('DOMContentLoaded', function () {
 	const $body = document.querySelector('body');
 	const $toggleBtn = document.querySelector('.toggle-menu');
 	const $pageHeder = document.querySelector('.page-header');
@@ -7,21 +7,21 @@ document.addEventListener('DOMContentLoaded',function () {
 	const $dropDownMenu = document.querySelector('#drop-down-menu');
 	const $main = document.querySelector('main')
 
-	$toggleBtn.addEventListener('click',function () {
+	$toggleBtn.addEventListener('click', function () {
 		$pageHeder.classList.toggle('page-heder-active-nav')
 		$main.classList.toggle('page-heder-active-nav')
 		$pageNav.classList.toggle('nav-active')
 		// when we remove class imediatly where will show white belt on the bottom coz our animation take 0.3ms
-		if($body.classList.contains('active-nav-body')) {
+		if ($body.classList.contains('active-nav-body')) {
 			setTimeout(() => {
 				$body.classList.toggle('active-nav-body')
-			},600)
+			}, 600)
 		} else {
 			$body.classList.toggle('active-nav-body')
 		}
 	})
 
-	$dropDownBtn.addEventListener('click',() => {
+	$dropDownBtn.addEventListener('click', () => {
 		$dropDownMenu.classList.toggle('active-drop-down-menu')
 	})
 })
